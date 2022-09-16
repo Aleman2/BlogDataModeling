08602c8c471e818b29b35c6786078f13f275d51e
Kaiki <alejandrobisuteria@gmail.com>
2022-09-16 18:02:01 +0000

    Diagram.vF

--- src/models.py
+++ src/models.py
@@ -5,29 +5,54 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
+import sqlalchemy as sa
+
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), unique=False, nullable=False)
+    username = Column(String(30), unique=True, nullable=False)
+    is_actived = Column( sa.Boolean(), nullable=False)
+
+
+class Planets(Base):
+    __tablename__ = 'planets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), unique=False, nullable=False)
 
-    def to_dict(self):
-        return {}
+class Persons(Base):
+    __tablename__ = 'persons'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), unique=False, nullable=False)
+
+
+class PersonsFavorite(Base):
+    __tablename__ = 'PersonsFavorite'
+    persons_id = Column(Integer, ForeignKey("persons.id"), primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
+
+
+
+class PlanetsFavorite(Base):
+    __tablename__ = 'PlanetsFavorite'
+    planet_id = Column(Integer, ForeignKey("planets.id"), primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
+
+ 
+        
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
